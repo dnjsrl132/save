0628f73f7859b10e342d1cbb8187fbe9be60b74d
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2021-12-14 12:29:46 +0900

    Add files via upload

---
+++ vs/echo_client/echo_client/echo_client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+
+#define BUF_SIZE 1024
+void ErrorHandling(char* message);
+
+int main()
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char name[BUF_SIZE];
+	char message[BUF_SIZE];
+	int str_len ,strLen;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("hSocket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servAddr.sin_port = htons(atoi("123654"));
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("connect() error!");
+	else
+		printf("Conneted........\n");
+
+	while (1) {
+		fputs("Input message(Q to quit): ", stdout);
+		fgets(message, BUF_SIZE, stdin);
+
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+
+		send(hSocket, message, strlen(message),0);
+
+		while ((strLen = recv(hSocket, name, 3, 0)) != 0) {
+			name[strLen] = 0;
+			if (!strcmp(name, "q"))
+				break;
+			str_len = recv(hSocket, message, BUF_SIZE - 1, 0);
+			message[str_len] = 0;
+			if (str_len == -1 || strLen == -1) break;
+			printf("Message from %s: %s", name, message);
+		}
+	}
+
+	closesocket(hSocket);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
